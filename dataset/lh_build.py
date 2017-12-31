2081356742e245642a081df7370769e6234feed3
Apple-jack <554290831@qq.com>
2017-12-31 23:22:06 +0800

    Cache 20 stocks, new load_data
    Cache 20 stocks' train data under ts=15.
    A new load_data_2 function for LSTM dataset.
    Add hs300 data.

--- dataset/lh_build.py
+++ dataset/lh_build.py
@@ -84,7 +84,7 @@ class DataManager:
         data = pd.read_csv(self.fns[0], encoding="gb2312")
         data = data[vars]
         data = np.array(data)
-        startIndex, endIndex = self.getDateIndex(data, startDate, endDate)
+        startIndex, endIndex = getDateIndex(data, startDate, endDate)
         data = data[endIndex: startIndex + 1, 1]        ## notice low index refer to newer data, we need to reverse it
         data = data[::-1]       ## reverse data
         data = np.expand_dims(data, 1)
@@ -94,7 +94,7 @@ class DataManager:
             temp = pd.read_csv(self.fns[i], encoding="gb2312")
             temp = temp[vars]
             temp = np.array(temp)
-            startIndex, endIndex = self.getDateIndex(temp, startDate, endDate)
+            startIndex, endIndex = getDateIndex(temp, startDate, endDate)
             temp = temp[endIndex: startIndex + 1, 1]
             temp = temp[::-1]
             temp = np.expand_dims(temp, 1)
@@ -109,16 +109,6 @@ class DataManager:
         np.save("matrix.npy", data.transpose())
         return 0
 
-    def getDateIndex(self, data, startDate, endDate):
-        startIndex = endIndex = 0
-        for i in range(data.shape[0]):
-            if data[i, 0] == startDate.strftime("%Y-%m-%d"):
-                startIndex = i
-                break
-            if data[i, 0] == endDate.strftime("%Y-%m-%d"):
-                endIndex = i
-        return startIndex, endIndex
-
     def test(self):
         names = os.listdir(self.src)
         path = os.path.join(self.src, names[0])
@@ -129,7 +119,32 @@ class DataManager:
         else:
             print("%s <= %s" % (date1, date2))
 
-def build_data(src, cache_src, target, timesteps, k, related_ts):
+def getDateIndex(data, startDate, endDate):
+    startIndex = endIndex = 0
+    for i in range(data.shape[0]):
+        if data[i, 0] == startDate.strftime("%Y-%m-%d"):
+            startIndex = i
+            break
+        if data[i, 0] == endDate.strftime("%Y-%m-%d"):
+            endIndex = i
+    return startIndex, endIndex
+
+def load_hs300():
+    startDate = datetime.strptime("2013-04-25", "%Y-%m-%d")
+    endDate = datetime.strptime("2017-05-15", "%Y-%m-%d")
+    vars = ["日期", "收盘价"]
+    data = pd.read_csv("000300.csv", encoding="gb2312")
+    data = data[vars]
+    data = np.array(data)
+    startIndex, endIndex = getDateIndex(data, startDate, endDate)
+    data = data[endIndex: startIndex + 1, 1]  ## notice low index refer to newer data, we need to reverse it
+    data = data[::-1]  ## reverse data
+    data = np.expand_dims(data, 1)
+    print(data.shape)
+    np.save("hs300.npy", data.transpose())
+    return data.transpose()
+
+def build_data(src, src_hs300, cache_src, target, timesteps, k, related_ts):
     ## using "matrix_no_zero.npy"
     ## src: data src
     ## cache_src: once data is loaded, it would be saved to cache
@@ -139,8 +154,10 @@ def build_data(src, cache_src, target, timesteps, k, related_ts):
     ## related_ts: how many timesteps are used to calculate correlation coefficient
 
     data = np.load(src)  ## shape=(total_stocks, total_timesteps)
-
     data_norm, max_data, min_data = data_normalization(data)
+
+    data_hs300 = np.load(src_hs300)
+    data_hs300_norm, hs300_max, hs300_min = data_normalization(data_hs300)
 
     cache_target = "%s/target%d_ts%d_k%d_rts%d.npy" % (cache_src, target, timesteps, k, related_ts)
     cache_targety = "%s/targety%d_ts%d_k%d_rts%d.npy" % (cache_src, target, timesteps, k, related_ts)
@@ -148,17 +165,23 @@ def build_data(src, cache_src, target, timesteps, k, related_ts):
     cache_posy = "%s/posy%d_ts%d_k%d_rts%d.npy" % (cache_src, target, timesteps, k, related_ts)
     cache_neg = "%s/neg%d_ts%d_k%d_rts%d.npy" % (cache_src, target, timesteps, k, related_ts)
     cache_negy = "%s/negy%d_ts%d_k%d_rts%d.npy" % (cache_src, target, timesteps, k, related_ts)
+    cache_hs300x = "%s/hs300x%d_ts%d_k%d_rts%d.npy" % (cache_src, target, timesteps, k, related_ts)
+    cache_hs300y = "%s/hs300y%d_ts%d_k%d_rts%d.npy" % (cache_src, target, timesteps, k, related_ts)
 
     if os.path.exists(cache_target) and os.path.exists(cache_targety) and \
-            os.path.exists(cache_pos) and os.path.exists(cache_neg) and os.path.exists(cache_posy) and os.path.exists(cache_negy):
+            os.path.exists(cache_pos) and os.path.exists(cache_neg) and os.path.exists(cache_posy) and os.path.exists(cache_negy)\
+            and os.path.exists(cache_hs300x) and os.path.exists(cache_hs300y):
         print("data exists")
-        return np.load(cache_target), np.load(cache_targety), np.load(cache_pos), np.load(cache_posy), np.load(cache_neg), np.load(cache_negy), max_data, min_data
+        return np.load(cache_target), np.load(cache_targety), np.load(cache_pos), np.load(cache_posy), np.load(cache_neg), \
+               np.load(cache_negy), max_data, min_data, np.load(cache_hs300x), np.load(cache_hs300y)
 
     print("data not exists")
 
     X_target_seg = data_norm[target, : -1]        ## shape=(total_timesteps - 1,)
     Y_target_seg = data_norm[target, 1:]          ## shape=(total_timesteps - 1,)
     XY_driving_seg = np.delete(data_norm, target, axis=0)    ## shape=(total_stocks - 1, total_timesteps)
+    X_hs300_seg = data_hs300_norm[0, : -1]        ## shape=(total_timesteps - 1,)
+    Y_hs300_seg = data_hs300_norm[0, 1:]          ## shape=(total_timesteps - 1,)
 
     n_samples = X_target_seg.shape[0] - related_ts + 1
     X_target = np.zeros(shape=(n_samples, related_ts))      ## shape=(n_samples, related_ts)
@@ -168,6 +191,8 @@ def build_data(src, cache_src, target, timesteps, k, related_ts):
     X_neg = np.zeros(shape=(n_samples, k, related_ts))      ## shape=(n_samples, top_k, realted_ts)
     Y_pos = np.zeros(shape=(n_samples, k, related_ts))      ## shape=(n_samples, top_k, related_ts)
     Y_neg = np.zeros(shape=(n_samples, k, related_ts))      ## shape=(n_samples, top_k, related_ts)
+    X_hs300 = np.zeros(shape=(n_samples, related_ts))
+    Y_hs300 = np.zeros(shape=(n_samples, related_ts))
 
     for i in range(X_target_seg.shape[0] - related_ts + 1):
         ## let time window slices on the total timesteps
@@ -175,11 +200,16 @@ def build_data(src, cache_src, target, timesteps, k, related_ts):
         Y_target[i] = Y_target_seg[i: i + related_ts]
         XY_driving[i] = XY_driving_seg[:, i: i + related_ts + 1]
         X_pos[i], X_neg[i], Y_pos[i], Y_neg[i] = get_pearson_related_data(X_target[i], XY_driving[i], k)
+        X_hs300[i] = X_hs300_seg[i: i + related_ts]
+        Y_hs300[i] = Y_hs300_seg[i: i + related_ts]
         ## calculate the difference
-        Y_target[i] = Y_target[i] - X_target[i]
+        # Y_target[i] = Y_target[i] - X_target[i]
+        # Y_hs300[i] = Y_hs300[i] - X_hs300[i]
 
     X_target = np.reshape(X_target, newshape=(X_target.shape[0], X_target.shape[1], 1))
     Y_target = np.reshape(Y_target, newshape=(Y_target.shape[0], Y_target.shape[1], 1))
+    X_hs300 = np.reshape(X_hs300, newshape=(X_hs300.shape[0], X_hs300.shape[1], 1))
+    Y_hs300 = np.reshape(Y_hs300, newshape=(Y_hs300.shape[0], Y_hs300.shape[1], 1))
 
     print("saving cache...")
     np.save(cache_target, X_target[:, -timesteps:])
@@ -188,8 +218,89 @@ def build_data(src, cache_src, target, timesteps, k, related_ts):
     np.save(cache_posy, Y_pos[:, :, -timesteps:])
     np.save(cache_neg, X_neg[:, :, -timesteps:])
     np.save(cache_negy, Y_neg[:, :, -timesteps:])
+    np.save(cache_hs300x, X_hs300[:, -timesteps:])
+    np.save(cache_hs300y, Y_hs300[:, -timesteps:])
 
-    return X_target[:, -timesteps:], Y_target[:, -timesteps:], X_pos[:, :, -timesteps:], Y_pos[:, :, -timesteps:], X_neg[:, :, -timesteps:], Y_neg[:, :, -timesteps:], max_data, min_data
+
+    return X_target[:, -timesteps:], Y_target[:, -timesteps:], X_pos[:, :, -timesteps:], Y_pos[:, :, -timesteps:], X_neg[:, :, -timesteps:], \
+           Y_neg[:, :, -timesteps:], max_data, min_data, X_hs300[:, -timesteps:], Y_hs300[:, -timesteps:]
+
+def load_data_2(src, src_hs300, timestep, return_sequence):
+    data = np.load(src)
+    data_hs300 = np.load(src_hs300)
+    data = np.concatenate((data, data_hs300), axis=0)
+    data_real = data[:, :]
+    data = data[:, :]
+
+    # # data normalization
+    max_data = np.max(data, axis=1)
+    min_data = np.min(data, axis=1)
+    max_data = np.reshape(max_data, (max_data.shape[0], 1))
+    min_data = np.reshape(min_data, (min_data.shape[0], 1))
+    data = (2 * data - (max_data + min_data)) / (max_data - min_data)
+
+    # dataset split
+    train_split = round(0.8 * data.shape[1])
+    val_split = round(0.9 * data.shape[1])
+
+    x_train_seg = data[:, : train_split]
+    y_train_seg = data[:, 1: train_split + 1]
+    x_val_seg = data[:, train_split: val_split]
+    y_val_seg = data[:, train_split + 1: val_split + 1]
+    x_test_seg = data[:, val_split: -1]
+    y_test_seg = data[:, val_split + 1:]
+    gt_test_seg = data_real[:, val_split + 1:]
+
+    n_train = (x_train_seg.shape[1] - timestep + 1) * x_train_seg.shape[0]
+    n_val = (x_val_seg.shape[1] - timestep + 1) * x_val_seg.shape[0]
+    n_test = (x_test_seg.shape[1] - timestep + 1) * x_test_seg.shape[0]
+    #LSTM
+    x_train = np.zeros(shape=(n_train, timestep))
+    y_train = np.zeros(shape=(n_train, timestep))
+    x_val = np.zeros(shape=(n_val, timestep))
+    y_val = np.zeros(shape=(n_val, timestep))
+    x_test = np.zeros(shape=(n_test, timestep))
+    y_test = np.zeros(shape=(n_test, timestep))
+    gt_test = np.zeros(shape=(n_test, timestep))
+
+    i = 0
+    for j in range(x_train_seg.shape[0]):
+        for k in range(x_train_seg.shape[1] - timestep + 1):
+            x_train[i, :] = x_train_seg[j, k: k + timestep]
+            y_train[i, :] = y_train_seg[j, k: k + timestep]
+            i += 1
+    assert i == n_train
+
+    i = 0
+    for j in range(x_val_seg.shape[0]):
+        for k in range(x_val_seg.shape[1] - timestep + 1):
+            x_val[i, :] = x_val_seg[j, k: k + timestep]
+            y_val[i, :] = y_val_seg[j, k: k + timestep]
+            i += 1
+    assert i == n_val
+
+    i = 0
+    for j in range(x_test_seg.shape[0]):
+        for k in range(x_test_seg.shape[1] - timestep + 1):
+            x_test[i, :] = x_test_seg[j, k: k + timestep]
+            y_test[i, :] = y_test_seg[j, k: k + timestep]
+            gt_test[i, :] = gt_test_seg[j, k: k + timestep]
+            i += 1
+    assert i == n_test
+
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_val = np.reshape(x_val, (x_val.shape[0], x_val.shape[1], 1))
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
+    y_val = np.reshape(y_val, (y_val.shape[0], y_val.shape[1], 1))
+    y_test = np.reshape(y_test, (y_test.shape[0], y_test.shape[1], 1))
+    gt_test = np.reshape(gt_test, (gt_test.shape[0], gt_test.shape[1], 1))
+
+    if return_sequence:
+        return [x_train, y_train, x_val, y_val, x_test, y_test, gt_test, max_data, min_data]
+    else:
+        return [x_train, y_train[:, -1], x_val, y_val[:, -1], x_test, y_test[:, -1], gt_test[:, -1], max_data, min_data]
 
 def data_normalization(data):
     ## rescale every row in data to [-1, 1]
@@ -261,9 +372,10 @@ if __name__ == "__main__":
     directory = "history_data"
     # dm = DataManager(directory)
     # dm.getMatrix(dm.commonFirstDate, dm.commonLastDate)
-    data = np.load("matrix.npy")
-    errors = np.load("errors.npy")
-    data_no_zero = np.load("matrix_no_zero.npy")
+    # data = np.load("matrix.npy")
+    # errors = np.load("errors.npy")
+    # data_no_zero = np.load("matrix_no_zero.npy")
+    # print(load_hs300())
     # print(data.shape)
     # print(len(errors))
     # count = 0
@@ -286,5 +398,3 @@ if __name__ == "__main__":
     # print(data.shape)
     # np.save("matrix_no_zero.npy", data)
     ################
-    print(data)
-    print(data_no_zero)
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import dataset.lh_build as build
 from keras.layers.core import *
 from keras.layers.recurrent import LSTM
 from keras.models import *
+from my_lstm import My_LSTM
 
 def exp_LSTM(niter, nsnapshot, ts, x_train, y_train, x_val, y_val, dim=1):
     x = Input(shape=(ts, dim))
-    lstm = LSTM(32, input_shape=(ts, dim), return_sequences=True)(x)
+    lstm = My_LSTM(32, input_shape=(ts, dim), return_sequences=True)(x)
+    # lstm = LSTM(32, return_sequences=True)(lstm)
     prediction = Dense(1)(lstm)
     model = Model(input=x, output=prediction)
     model.compile('rmsprop', 'mse')
@@ -60,31 +62,42 @@ def exp_LSTM(niter, nsnapshot, ts, x_train, y_train, x_val, y_val, dim=1):
 
 if __name__ == "__main__":
     src = "dataset/matrix_no_zero.npy"
+    src_hs300 = "dataset/hs300.npy"
     data_cache = "cache"
     train_split = 0.8
     val_split = 0.9
 
-    X_target, Y_target, X_pos, Y_pos, X_neg, Y_neg, max_data, min_data = build.build_data(src, data_cache, 0, 15, 20, 30)
+    X_target, Y_target, X_pos, Y_pos, X_neg, Y_neg, max_data, min_data, X_hs300, Y_hs300 = build.build_data(src, src_hs300, data_cache, target=0, timesteps=15, k=20, related_ts=30)
+    x_train, y_train, x_val, y_val, x_test, y_test, gt_test, max_data, min_data = build.load_data_2(src, src_hs300, 15, True)
 
-    X_pos = np.transpose(X_pos, (0, 2, 1))
-    X_neg = np.transpose(X_neg, (0, 2, 1))
-    Y_pos = np.transpose(Y_pos, (0, 2, 1))
-    Y_neg = np.transpose(Y_neg, (0, 2, 1))
+    print(X_target[0].flatten())
+    print('----------')
+    print(x_train[15].flatten())
 
-    train_border = round(train_split * X_target.shape[0])
-    val_border = round(val_split * X_target.shape[0])
+    # # cache data
+    # for i in range(20):
+    #     print("start loading target: %d" % i)
+    #     build.build_data(src, data_cache, target=i, timesteps=15, k=10, related_ts=30)
 
-    X_train = X_target[: train_border, :, :]
-    Y_train = Y_target[: train_border, :]
-
-    for i in range(X_pos.shape[2]):
-        X_train = np.concatenate((X_train, X_pos[: train_border, :, i: i+1]), axis=0)
-        X_train = np.concatenate((X_train, X_neg[: train_border, :, i: i+1]), axis=0)
-        Y_train = np.concatenate((Y_train, Y_pos[: train_border, :, i: i+1]), axis=0)
-        Y_train = np.concatenate((Y_train, Y_neg[: train_border, :, i: i+1]), axis=0)
-
-    X_val = X_target[train_border: val_border, :, :]
-    Y_val = Y_target[train_border: val_border, :]
-
-    exp_LSTM(100, 5, 15, X_train, Y_train, X_val, Y_val, dim=X_train.shape[2])
+    # X_pos = np.transpose(X_pos, (0, 2, 1))
+    # X_neg = np.transpose(X_neg, (0, 2, 1))
+    # Y_pos = np.transpose(Y_pos, (0, 2, 1))
+    # Y_neg = np.transpose(Y_neg, (0, 2, 1))
+    #
+    # train_border = round(train_split * X_target.shape[0])
+    # val_border = round(val_split * X_target.shape[0])
+    #
+    # X_train = X_target[: train_border, :, :]
+    # Y_train = Y_target[: train_border, :]
+    #
+    # for i in range(X_pos.shape[2]):
+    #     X_train = np.concatenate((X_train, X_pos[: train_border, :, i: i+1]), axis=0)
+    #     X_train = np.concatenate((X_train, X_neg[: train_border, :, i: i+1]), axis=0)
+    #     Y_train = np.concatenate((Y_train, Y_pos[: train_border, :, i: i+1]), axis=0)
+    #     Y_train = np.concatenate((Y_train, Y_neg[: train_border, :, i: i+1]), axis=0)
+    #
+    # X_val = X_target[train_border: val_border, :, :]
+    # Y_val = Y_target[train_border: val_border, :]
+    #
+    # exp_LSTM(100, 5, 15, X_train, Y_train, X_val, Y_val, dim=X_train.shape[2])
 
